b50af26b78871ed572aea39183aa2249923c01e1
tuantla <tuantla80@gmail.com>
2019-04-08 16:55:53 +0900

    init

--- run.sh
+++ run.sh
@@ -2,8 +2,10 @@
 
 git status
 
+read -r -p 'Commit message: ' desc  # prompt user for commit message
+
 git add .
 
-git commit -m "init"
+git commit -m "$desc"               # commit with message
 
 git push
