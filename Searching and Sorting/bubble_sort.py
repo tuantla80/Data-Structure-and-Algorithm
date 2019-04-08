cfd89da7fd48180036a2351c19a77464816628d8
tuantla <tuantla80@gmail.com>
2019-04-08 16:48:08 +0900

    init

--- Searching and Sorting/bubble_sort.py
+++ Searching and Sorting/bubble_sort.py
@@ -19,19 +19,18 @@ Note on swap (a, b)
     a, b = b, a
 """
 
-def bubble_sort(arr):
 
-    for i in list(range(len(arr)-1, 0, -1)):
+def bubble_sort(arr):
+    for i in list(range(len(arr) - 1, 0, -1)):
         # n =len(arr) --> i = [n-1, n-2, n-3, 1]
         # since the max value is at the end of list, and so on
         for j in range(i):  # j run from 0 to i-1
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr
+
 
 if __name__ == "__main__":
     arr = [1, 2, 5, 0, 4]
     new_arr = bubble_sort(arr)
     print('after bubble sort', new_arr)
-
-
---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git status
+
+git add .
+
+git commit -m "init"
+
+git push
